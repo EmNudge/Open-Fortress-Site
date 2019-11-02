781a10e1105ffd5ccf167cc651a0a632b9c5ec90
calvin.kipperman@gmail.com <emnudge@gmail.com>
2019-11-02 19:28:07 -0400

    added text to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Kill 'Em Dead</h1>
+    <p>Open Fortress is a free and open-source passion project, lovingly crafted by nearly a hundred members of the Team Fortress community with an ideal of fun gameplay as well as maximum customizability.</p>
+
+    <h2>Meet The Merc</h2>
+    <p>A smooth talker, hard hitter and a hell of a fast shooter, the Mercenary is a man raised by television and nurtured by film. Moulded by the dashing gunslingers and suave secret agents of the silver screen, he aspired to be just like them, even from a young age, when he went fist-to-fist with the orphanage's worst bullies and came out on top.</p>
+    <p>With an arsenal jam-packed with the finest experimental prototype weapons blood-sport can buy, he's ready to show the world that he really is that twelve on the one-to-ten scale of badass.</p>
   </Layout>
 )
 
