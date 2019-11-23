ead7a117dcdcf302b4f240e67119d87b7b8a018f
calvin.kipperman@gmail.com <emnudge@gmail.com>
2019-11-23 16:48:21 -0500

    added AWS testing in order to test on netlify due to continuous  deployment

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -9,6 +9,18 @@ const NotFoundPage = () => {
   const credits = useCredits();
   console.log(credits);
 
+  const steamIDs = [76561198128576895,76561198006395451,76561198080213691,76561198147116054]
+  fetch('/.netlify/functions/get-steam-avatars', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({steamIDs}),
+  }).then(res => {
+    return res.json()
+  }).then(console.log)
+
   return (
     <Layout>
       <SEO title="Credits" />
