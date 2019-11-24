263f18262d5a266cf14beac09dc5a0012f1dec87
emnudge <emnudge@gmail.com>
2019-11-24 18:23:32 -0500

    abstracted the avatar to parent component

--- src/components/steamProfiles/index.js
+++ src/components/steamProfiles/index.js
@@ -3,12 +3,16 @@ import SteamProfile from "./steamProfileBanner"
 import "./index.scss"
 
 const SteamProfiles = ({ credits, categories }) => {
-
+  const getSteamProfile = credit => {
+    const [name, info] = credit;
+    info.name = name;
+    if (!info.avatar) info.avatar = "https://icon-library.net/images/steam-question-mark-icon/steam-question-mark-icon-29.jpg";
+    return <SteamProfile key={name} {...info} />
+  }
+  
   return (
     <div className="steam-profile-container">
-      {[...credits].map(([name, info]) => (
-        <SteamProfile key={name} name={name} {...info} />
-      ))}
+      {[...credits].map(getSteamProfile)}
     </div>
   )
 }
