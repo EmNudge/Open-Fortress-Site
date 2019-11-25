0047227e49138fe9548015d1abeecb9d61d4d237
emnudge <emnudge@gmail.com>
2019-11-24 22:14:09 -0500

    added netlify dev and moved hook to utils to use as async

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -3,14 +3,17 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import useCredits from "../hooks/useCredits"
-import useSteamAPI from "../hooks/useSteamAPI"
+import getWithAvatars from "../utils/getWithAvatars"
 import SteamProfiles from "../components/steamProfiles"
 
 const Credits = () => {
   const [credits, setCredits] = React.useState(useCredits())
 
   React.useEffect(() => {
-    useSteamAPI(credits).then(setCredits)
+    (async () => {
+      const newCredits = await getWithAvatars(credits);
+      setCredits(newCredits);
+    })()
   }, [])
 
   return (
